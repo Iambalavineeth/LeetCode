f88582e7ef5eefff1efd69bd79ee8aef0c6b7252
Bala Vineeth Netha Thatipamula <bthatip@clemson.edu>
2017-10-06 16:29:13 -0400

    Changed ComplexNumberMultiplication file

--- ComplexNumberMultiplication.java
+++ ComplexNumberMultiplication.java
@@ -1,19 +1,20 @@
 public class ComplexNumberMultiplication{
-  public static void main(String[] args) {
-    String input1 = "1+1i";
-    String input2 = "1+1i";
 
+  public static String complexMultiply(String input1, String input2){
     String[] parts1 = input1.split("\\+");
     String[] parts2 = input2.split("\\+");
 
     int real  = ((Integer.parseInt(parts1[0]) * Integer.parseInt(parts2[0])) - (Integer.parseInt(parts1[1].substring(0, parts1[1].length()-1)) * Integer.parseInt(parts2[1].substring(0, parts2[1].length()-1))));
     int complex = ((Integer.parseInt(parts1[0]) * Integer.parseInt(parts2[1].substring(0, parts2[1].length()-1))) + (Integer.parseInt(parts1[1].substring(0, parts1[1].length()-1)) * Integer.parseInt(parts2[0])));
-    System.out.println(real);
-    System.out.println(complex);
 
-    String realStr =  Integer.toString(real);
-    String complexStr = Integer.toString(complex);
-    String result = realStr + "+" + complexStr + "i";
-    System.out.println(result);
+    String result = Integer.toString(real) + "+" + Integer.toString(complex) + "i";
+    return result;
+  }
+
+  public static void main(String[] args) {
+    String input1 = "1+1i";
+    String input2 = "1+1i";
+
+    System.out.println(ComplexNumberMultiplication.complexMultiply(input1, input2));
   }
 }
